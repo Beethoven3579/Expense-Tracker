1a9d5d863ba2cdcd9e3c03241cf7ebc61f9ea020
nickpaolino <nick_paolino@yahoo.com>
2021-04-18 19:51:21 -0400

    expense stays on the DOM

--- app.js
+++ app.js
@@ -4,17 +4,27 @@ document.getElementById('add-btn')
 .addEventListener('click', (e) => {
     e.preventDefault();
 
-  addExpense();
+    const date = document.getElementById('expense-date');
+    const type = document.getElementById('expense-type');
+    const description = document.getElementById('expense-description');
+    const location = document.getElementById('expense-location');
+    const amount = document.getElementById('expense-amount');
+
+    const expenseItem = {
+        id: Date.now(),
+        expenseType: type.value,
+        expenseDate: date.value,
+        expenseDescription: description.value,
+        expenseLocation: location.value,
+        expenseAmount: amount.value
+        
+    }
+  addExpense(expenseItem);
 })
 
 
 
-function newExpense(expenseItem) {
-    
-    const date = document.getElementById('expense-date');
-    const type = document.getElementById('expense-type');
-    const description = document.getElementById('expense-description');
-    const amount = document.getElementById('expense-amount');
+function newExpense(expense) {
    
     const expenseList = document.getElementById('expense-list');
     const row = expenseList.insertRow(1);
@@ -22,12 +32,14 @@ function newExpense(expenseItem) {
     const cell2 = row.insertCell(1);
     const cell3 = row.insertCell(2);
     const cell4 = row.insertCell(3);
+    const cell5 = row.insertCell(4);
 
-    cell1.textContent = type.value;
-    cell2.textContent = date.value;
-    cell3.textContent = description.value;
-    cell4.textContent = `$${amount.value}`;
-    cell4.style.color = 'red';
+    cell1.textContent = expense.expenseType
+    cell2.textContent = expense.expenseDate
+    cell3.textContent = expense.expenseDescription
+    cell4.textContent = expense.expenseLocation
+    cell5.textContent = `$${expense.expenseAmount}`;
+    cell5.style.color = 'red';
     
     const deleteButton = document.createElement('button');
     deleteButton.textContent = 'Remove';
@@ -36,36 +48,30 @@ function newExpense(expenseItem) {
 
     deleteButton.addEventListener('click', (e) => {
         e.preventDefault();
-        deleteExpense(deleteButton, expenseItem);
+        deleteExpense(deleteButton, expense);
     })
 
-     type.value = '';
-     date.value = '';
-     description.value = '';
-     amount.value = '';
-}
+    document.getElementById('form').reset();
+ }
 
 function addExpense(expense) {
-    const expenseItem = {
-        id: Math.random(),
-        expense: expense
-    }
-    newExpense(expenseItem);
-    expenseArray.push(expenseItem);
-    addToLocalStorage(expenseItem);
+    
+    newExpense(expense);
+    expenseArray.push(expense);
+    addToLocalStorage(expense);
 }
 
-function deleteExpense(deleteButton, expenseItem) {
+function deleteExpense(deleteButton, expense) {
     deleteButton.parentElement.remove();
     for (let i = 0; i < expenseArray.length; i++) {
-        if (expenseArray[i].id === expenseItem.id) {
+        if (expenseArray[i].id === expense.id) {
         expenseArray.splice(i, 1);
         localStorage.setItem('expenseArray', JSON.stringify(expenseArray));
         }
     }
 }
 
-function addToLocalStorage(expenseItem) {
+function addToLocalStorage(expense) {
     localStorage.setItem('expenseArray', JSON.stringify(expenseArray));
 }
 
